82536bd133fab94656c269327af1f2f896fcef50
bunganesha <bunga.nesha10@gmail.com>
2026-06-01 19:52:41 +0700

    Menambahkan fitur dropdown distribusi dan pengelolaan dashboard

--- mbglokal/src/main/java/com/kelompok7/mbglokal/entity/DetailMenu.java
+++ mbglokal/src/main/java/com/kelompok7/mbglokal/entity/DetailMenu.java
@@ -1,5 +1,7 @@
 package com.kelompok7.mbglokal.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -21,6 +23,7 @@ public class DetailMenu {
 
     @ManyToOne
     @JoinColumn(name = "id_menu")
+    @JsonIgnore
     private PaketMenu paketMenu;
 
     @ManyToOne
